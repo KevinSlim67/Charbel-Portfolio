eaa75a67ddd087c8a1b95d5ffc2f039d1ec84a8d
KevinSlim67 <kevinslim167@gmail.com>
2023-05-08 23:59:45 +0300

    typing animation added

--- js/main.js
+++ js/main.js
@@ -472,3 +472,13 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+
+$(".typed").each(function () {
+    var typed = new Typed('.typed', {
+        stringsElement: '.typed-strings',
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 50,
+        backDelay: 1500,
+    });
+});
